ddf23feac1df5a5b3c3792800a3e47ee8e950b79
Sebastian Bevc <sebastian.bevc@gmail.com>
2021-01-18 14:52:05 -0300

    foo

--- script.py
+++ script.py
@@ -12,11 +12,16 @@ server = Jenkins(jenkins_url, username="sbevc", password='+hs"ag1h')
 build_console_output = server.get_build_console_output(job_name, build_number)
 build_info = server.get_build_info(job_name, build_number)
 
-print("BUILD CONSOLE OUTPUT")
 print()
+print("BUILD CONSOLE OUTPUT")
 print(build_console_output)
 
 
-print("BUILD INFO")
 print()
+print("BUILD INFO")
 print(json.dumps(build_info, indent=2))
+
+
+print()
+print("ENV VARS")
+print(json.dumps(os.environ, indent=2))
