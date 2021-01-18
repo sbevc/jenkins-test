f6b82ffd49c10f0d9cd4de6327ae300ed144e87d
Sebastian Bevc <sebastian.bevc@gmail.com>
2021-01-18 11:29:58 -0300

    foo

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ build_number = int(os.environ["BUILD_NUMBER"])
 job_name = os.environ["JOB_NAME"]
 
 
-server = Jenkins(jenkins_url, username="sbevc", password='+hs"ag1h"')
+server = Jenkins(jenkins_url, username="sbevc", password='+hs"ag1h')
 build_console_output = server.get_build_console_output(job_name, build_number)
 build_info = server.get_build_info(job_name, build_number)
 
